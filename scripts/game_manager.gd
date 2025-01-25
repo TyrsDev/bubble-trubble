c3b47b01f7de33fff93bc69a686e7d7a239d9452
Johannes Hoseth Klev <tyrsdev@gmail.com>
2025-01-25 13:28:57 +0100

    replace restart functionality in game_manager with level_manager

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,12 +1,11 @@
 extends Node
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	# Check if Spacebar is pressed
-	if Input.is_action_just_pressed("ui_accept"): # "ui_accept" is mapped to Spacebar by default
-		restart_scene()
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
 
-func restart_scene():
-	# Get the current scene path and reload it
-	get_tree().reload_current_scene()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
