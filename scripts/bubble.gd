c1eb31bf8254f65181a9e222de8cbb69a478b037
magnusnor <magnus.nordahl@bekk.no>
2025-01-26 15:03:17 +0100

    Apply enemy damage to bubbles

--- scripts/bubble.gd
+++ scripts/bubble.gd
@@ -4,8 +4,13 @@ extends CharacterBody2D
 @export var hp: int
 @export var speed: int
 
+@onready var sprite = $Sprite2D
+@onready var animation = $AnimatedSprite2D
+
 var path: Path2D
 var progress: float = 0.0
+
+var is_dead: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -25,3 +30,16 @@ func _process(delta: float) -> void:
 func set_path(new_path: Path2D) -> void:
 	path = new_path
 	progress = 0.0 # Start at beginning of path
+
+func apply_damage(amount: int):
+	if is_dead:
+		return
+	
+	hp -= amount		
+	
+	if hp <= 0:
+		pop()
+
+func pop():
+	is_dead = true
+	queue_free()
--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -3,9 +3,12 @@ extends Node
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
 @onready var detection_collision_shape = $Vision/CollisionShape2D
+@onready var aoe_area = $Vision
 
 @export_group("Enemy Properties")
 @export var detection_radius: float = 16.0
+@export var damage_amount: int = 1
+@export var damage_frame: int = 5  # Frame to apply damage
 
 var bubble_count = 0
 
@@ -13,7 +16,6 @@ var bubble_count = 0
 func _ready() -> void:
 	if detection_collision_shape.shape is CircleShape2D:
 		detection_collision_shape.shape.radius = detection_radius
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -32,9 +34,19 @@ func _on_vision_body_exited(body: Node2D) -> void:
 		bubble_count -= 1
 		_update_animation()
 
+func _on_animated_sprite_2d_frame_changed() -> void:
+	if animated_sprite_2d.animation == "attack":
+		if animated_sprite_2d.frame == damage_frame:
+			apply_aoe_damage()
 
 func _update_animation():
 	if (bubble_count > 0):
 		animated_sprite_2d.animation = "attack"
 	else:
 		animated_sprite_2d.animation = "idle"
+
+func apply_aoe_damage():
+	var overlapping_bubbles = aoe_area.get_overlapping_bodies()
+	for bubble in overlapping_bubbles:
+		if bubble.is_in_group("bubble"):
+			bubble.apply_damage(damage_amount)
