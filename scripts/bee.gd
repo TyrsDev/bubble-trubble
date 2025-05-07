4daa21e1674909b40b8723bebe3d74a09db04b81
Johannes Klev <tyrsdev@gmail.com>
2025-05-07 17:08:23 +0200

    attack loops no longer intrude on target acquisition logic

--- scripts/bee.gd
+++ scripts/bee.gd
@@ -11,79 +11,98 @@ extends Node2D
 @export var attack_cooldown: float = 2.0
 @export var projectile_speed: float = 100.0
 
+# Store original stinger offset
+var stinger_offset = Vector2.ZERO
+
 var current_target: Node2D = null
 var can_attack: bool = true
 var attack_timer: float = 0.0
 var is_attacking: bool = false
-
-# Store original stinger offset
-var stinger_offset = Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if detection_collision_shape.shape is CircleShape2D:
 		detection_collision_shape.shape.radius = detection_radius
 	animated_sprite_2d.frame_changed.connect(_on_frame_changed)
+	animated_sprite_2d.animation_finished.connect(_on_body_animation_finished)
 	
 	# Save original stinger position
 	stinger_offset = stinger_point.position
+	
+	# Make sure the default animation loops
+	if not animated_sprite_2d.sprite_frames.get_animation_loop("default"):
+		print("Warning: Default animation should be set to loop!")
 
 func _process(delta: float) -> void:
+	if current_target and is_instance_valid(current_target):
+		face_target(current_target)
+	
 	if not can_attack:
 		attack_timer += delta
 		if attack_timer >= attack_cooldown:
 			can_attack = true
 			attack_timer = 0.0
 	
-	if current_target and can_attack and not is_attacking:
+	# If we're not attacking and we have a valid target, start a new attack
+	if current_target and is_instance_valid(current_target) and can_attack and not is_attacking:
 		start_attack()
+	elif not is_instance_valid(current_target) and not is_attacking:
+		# If we don't have a target and we're not in an attack animation, make sure we're in default
+		if animated_sprite_2d.animation != "default":
+			animated_sprite_2d.play("default")
+
+# Face the target by flipping sprite appropriately
+func face_target(target: Node2D) -> void:
+	var direction = (target.global_position - global_position).normalized()
+	if direction.x < 0:
+		animated_sprite_2d.flip_h = false
+		stinger_point.position = stinger_offset
+	else:
+		animated_sprite_2d.flip_h = true
+		stinger_point.position = Vector2(-stinger_offset.x, stinger_offset.y)
 
 func _on_vision_body_entered(body: Node2D) -> void:
 	if body.is_in_group("bubble") and current_target == null:
 		current_target = body
-		# Face the target without rotation
-		var direction = (body.global_position - global_position).normalized()
-		if direction.x < 0:
-			animated_sprite_2d.play("default")
-			animated_sprite_2d.flip_h = false
-		else:
-			animated_sprite_2d.play("default")
-			animated_sprite_2d.flip_h = true
 
 func _on_vision_body_exited(body: Node2D) -> void:
 	if body == current_target:
+		print("Target exited vision: ", body)
 		current_target = null
-		animated_sprite_2d.play("default")
+		# Try to find a new target immediately when current one leaves
+		find_new_target()
+
+# Add a new function to find targets
+func find_new_target() -> bool:
+	print("Looking for new target...")
+	var bodies = vision_area.get_overlapping_bodies()
+	for body in bodies:
+		if body.is_in_group("bubble"):
+			print("Found new target: ", body)
+			current_target = body
+			return true
+	return false
 
 func start_attack():
 	can_attack = false
 	is_attacking = true
-	# Determine attack direction and play appropriate animation
-	var direction = (current_target.global_position - global_position).normalized()
-	if direction.x < 0:
-		animated_sprite_2d.play("attack_left")
-	else:
-		animated_sprite_2d.play("attack_right")
+	
+	# Always play attack_left due to sprite being flipped when facing right
+	animated_sprite_2d.play("attack_left")
 
 func _on_frame_changed() -> void:
+	# If we're attacking and reach frame 2, try to spawn a projectile
 	if is_attacking and animated_sprite_2d.frame == 2:
-		if is_instance_valid(current_target):
-			# First flip the sprite based on the direction of the target
-			var direction = (current_target.global_position - global_position).normalized()
-			if direction.x < 0:
-				animated_sprite_2d.flip_h = false
-				stinger_point.position = stinger_offset
-			else:
-				animated_sprite_2d.flip_h = true
-				stinger_point.position = Vector2(-stinger_offset.x, stinger_offset.y)
-			
-			# Then spawn the projectile after flipping
+		# Only spawn projectile if target is valid
+		if current_target and is_instance_valid(current_target):
+			print("Spawning projectile at target: ", current_target)
 			spawn_projectile()
 		else:
-			# Target was destroyed, reset attack state
-			is_attacking = false
-			can_attack = true
-			animated_sprite_2d.play("default")
+			print("No valid target for projectile")
+			# If we somehow lost the target mid-attack, try to find a new one
+			if find_new_target() and is_attacking:
+				print("Found new target mid-attack, spawning projectile")
+				spawn_projectile()
 
 func spawn_projectile() -> void:
 	var projectile = preload("res://scenes/enemies/bee_projectile.tscn").instantiate()
@@ -91,7 +110,25 @@ func spawn_projectile() -> void:
 	projectile.global_position = stinger_point.global_position
 	projectile.setup(current_target, projectile_speed, damage_amount)
 
-func _on_animation_finished():
+func _on_body_animation_finished():
+	# Print animation name
+	print(animated_sprite_2d.animation + " finished.")
+	
+	# If this was an attack animation
 	if animated_sprite_2d.animation.begins_with("attack"):
 		is_attacking = false
-		animated_sprite_2d.play("default")
+		can_attack = true # Make sure we can attack again
+		
+		# Check if we still have a valid target
+		if current_target and is_instance_valid(current_target):
+			print("Target still valid after attack, starting new attack")
+			if can_attack: # Double-check we can attack
+				start_attack()
+		else:
+			# Otherwise go back to default animation
+			animated_sprite_2d.play("default")
+			
+			# Look for a new target
+			if find_new_target() and can_attack:
+				print("Found new target after attack, starting new attack")
+				start_attack()
